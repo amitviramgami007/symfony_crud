e938e6e7e4702067a806e097e4dd3040f46b0f98
Amit Viramgami <amitviramgami@gmail.com>
2021-04-06 19:39:34 +0530

    Create Home Controller and make hello user funtion

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/home", name="home")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/helloUser/{name?}", name="hello_user")
+     */
+    public function helloUser(Request $request, $name)
+    {
+        // request
+        // $name = $request->get('name');
+        return $this->render('home/greet.html.twig', compact('name'));
+    }
+}
